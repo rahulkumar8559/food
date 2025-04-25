e2d7f7d0d05a20120f2e27c50c5f037838617ad9
Rahul Kumar <rk681253@gmail.com>
2025-04-25 17:15:32 +0530

    navbar

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,8 +4,8 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='text-[#d9d9d9] bg-[#323232] flex flex-col items-center gap-[20px] mt-[100px] p-[8vw] p-[20px] pt-[80px]'>
-        <div className='w-[100%] gap-[80px] ' style={{ display:'grid', gridTemplateColumns:"2fr 1fr 1fr" }} >
-            <div className='flex flex-col  gap-[20px]  items-start'>
+        <div className='w-[100%] gap-[80px]  block sm:grid' style={{  gridTemplateColumns:"2fr 1fr 1fr" }} >
+            <div className='flex flex-col mb-[20px] gap-[20px]  items-start'>
                 <img src={assets.logo} alt="" />
                 <p className='text-[14px]'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum quas vel rem beatae. Rerum libero adipisci cupiditate eaque mollitia earum quaerat commodi, non, vel tempora consequuntur, omnis et iste</p>
                 <div className='flex'>
