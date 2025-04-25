8dbba84df73423fd73916c69070a81a31b962ccf
Rahul Kumar <rk681253@gmail.com>
2025-04-25 19:16:38 +0530

    deploy

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='h-[35vw] bg-no-repeat m-[30px auto] pt-[20px] bg-contain relative rounded-[20px] ' style={{backgroundImage:`url('header_img.png')`}}>
+    <div className='h-[35vw] bg-no-repeat m-[30px auto] pt-[20px] bg-contain relative rounded-[20px] ' style={{backgroundImage:`url('/header_img.png')`}}>
         <div className=' md:max-w-[55%] absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw] animate-fade-in '  >
             <h2 className=' sm:font-[500] text-[14px] font-[400]  text-white sm:text-[max(4.5vw,22px)]'>Order Your Favourite Food Here</h2>
             <p className=' hidden sm:block text-white text-[1.5vw]'>Choose from a diverse menu featuring a delectable array of dishes crafted with the </p>
